8ce583bb35c160080e2b1e45280c60e6165643cf
Justin Cook <juscook10@gmail.com>
2017-02-18 21:53:13 -0600

    Tons of updates

---
+++ Pokemon/CoreDataHelp.swift
@@ -0,0 +1,73 @@
+//
+//  CoreDataHelp.swift
+//  Pokemon
+//
+//  Created by Justin Cook on 2/18/17.
+//  Copyright © 2017 Justin Allen Designs. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+func addAllPokemon() {
+    createPokemon(name: "Mew", imageName: "mew")
+    createPokemon(name: "Meowth", imageName: "meowth")
+    createPokemon(name: "Pikachu", imageName: "pikachu")
+    createPokemon(name: "Rattata", imageName: "rattata")
+    createPokemon(name: "Squirtle", imageName: "squirtle")
+    
+    (UIApplication.shared.delegate as! AppDelegate).saveContext()
+    
+}
+
+func createPokemon(name : String, imageName: String) {
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    let pokemon = Pokemon(context: context)
+    pokemon.name = name
+    pokemon.imageName = imageName
+}
+
+func getAllPokemon() -> [Pokemon] {
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    do {
+        let pokemons = try context.fetch(Pokemon.fetchRequest()) as! [Pokemon]
+        
+        if pokemons.count == 0 {
+            addAllPokemon()
+            return getAllPokemon()
+        }
+        
+        return pokemons
+    } catch {}
+    
+    return []
+}
+
+func getAllCaughtPokemons() -> [Pokemon] {
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    let fetchRequest = Pokemon.fetchRequest() as NSFetchRequest<Pokemon>
+    fetchRequest.predicate = NSPredicate(format: "caught == %@", true as CVarArg)
+    
+    do {
+        let pokemons = try context.fetch(fetchRequest) as [Pokemon]
+        return pokemons
+    } catch {}
+    return []
+}
+
+func getAllUnCaughtPokemons() -> [Pokemon] {
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    let fetchRequest = Pokemon.fetchRequest() as NSFetchRequest<Pokemon>
+    fetchRequest.predicate = NSPredicate(format: "caught == %@", false as CVarArg)
+    
+    do {
+        let pokemons = try context.fetch(fetchRequest) as [Pokemon]
+        return pokemons
+    } catch {}
+    return []
+}
+
--- Pokemon/ViewController.swift
+++ Pokemon/ViewController.swift
@@ -7,19 +7,93 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var updateCount = 0
+    
+    var manager = CLLocationManager()
+    
+    var pokemons : [Pokemon] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        pokemons = getAllPokemon()
+        
+        manager.delegate = self
+        
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            mapView.delegate = self
+            mapView.showsUserLocation = true
+            manager.startUpdatingLocation()
+            
+            Timer.scheduledTimer(withTimeInterval: 5, repeats: true, block: { (Timer) in
+                //Spawn a Pokemon
+                if let coord = self.manager.location?.coordinate {
+                    let anno = MKPointAnnotation()
+                    anno.coordinate = coord
+                    let randoLat = (Double(arc4random_uniform(200))-100.0)/50000.0
+                    let randoLon = (Double(arc4random_uniform(200))-100.0)/50000.0
+                    anno.coordinate.latitude += randoLat
+                    anno.coordinate.longitude += randoLon
+                    self.mapView.addAnnotation(anno)
+                }
+                
+            })
+        } else {
+           manager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        if annotation is MKUserLocation {
+            let annoView = MKAnnotationView(annotation: annotation, reuseIdentifier: nil)
+            
+            annoView.image = UIImage(named: "player")
+            
+            var frame = annoView.frame
+            frame.size.height = 50
+            frame.size.width = 50
+            annoView.frame = frame
+            
+            return annoView
+        }
+        
+        let annoView = MKAnnotationView(annotation: annotation, reuseIdentifier: nil)
+        
+        annoView.image = UIImage(named: "mew")
+        
+        var frame = annoView.frame
+        frame.size.height = 50
+        frame.size.width = 50
+        annoView.frame = frame
+        
+        return annoView
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        if updateCount < 3 {
+            let region = MKCoordinateRegionMakeWithDistance(manager.location!.coordinate, 400, 400)
+            mapView.setRegion(region, animated: false)
+            updateCount += 1
+        } else {
+            manager.stopUpdatingLocation()
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func centerTapped(_ sender: Any) {
+        if let coord = manager.location?.coordinate {
+            let region = MKCoordinateRegionMakeWithDistance(manager.location!.coordinate, 400, 400)
+            mapView.setRegion(region, animated: true)
+        }
+        
     }
-
-
+    
 }
 
